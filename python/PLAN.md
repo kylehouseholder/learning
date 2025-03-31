258d31bd68f005da2a33f36527ae9cec5646aac0
kmh <kylemhouseholder@gmail.com>
2025-03-30 23:04:01 -0500

    retry: added to calc, made planfile

---
+++ python/PLAN.md
@@ -0,0 +1,11 @@
+# Working Title: AutoPilot Calculator
+
+Multifunction calculator for aircraft performance and limitations.
+- Density altitude
+- Takeoff & landing distance
+- Climb performance:
+  - Time, fuel, & distance to climb
+  - Climb gradient & rate
+- Cross country calculations:
+  - Per-leg timing & fuel
+  - Wind correction
---
+++ python/calcDA.py
@@ -0,0 +1,54 @@
+import math
+
+def getval(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Try again, but with a number: ")
+
+# Make a list of temperature at altitude from test.txt
+altm = getval("Enter altitude (ft): ")
+if altm > 40000:
+    print("Altitude is too high. Try again.")
+    altm = getval("Enter altitude (ft): ")
+
+baro = getval("Enter barometer setting (\"Hg): ")
+if baro < 25.5 or baro > 32.0:
+    print("Barometer setting out of range, try again.")
+    baro = getval("Enter barometer setting (\"Hg): ")
+
+temp = getval("Enter temperature (°C): ")
+if temp < -40 or temp > 60:
+    print("Temperature exceeds range (-40°C to 60°C), try again.")
+    temp = getval("Enter temperature (°C): ")
+
+altoffset = (29.92 - baro) * 1000
+altp = altm + altoffset
+
+# !F: Rudimentary ISA calculation - replace
+tempISA = 15 + (altm/1000 * -2)
+tempoffset = temp - tempISA
+
+#  !F: sanity check
+print("Pressure altitude: ")
+print(altp)
+print("ISA Temp: ")
+print(tempISA)
+
+# !F: Not a working exit, needs return to menu and exit
+exit = getval("Exit? Y/N")
+
+# Calculate density altitude
+
+# Calculate takeoff and landing distances, both calm and with at least two wind depths (user set)
+
+# Calculate climb gradient, climb rate, time to climb to user-driven cruise target
+
+# Calculate estimated HP% and performance in cruise
+
+# Calculate VNAV-style descent profile
+
+
+
+
