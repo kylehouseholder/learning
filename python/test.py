fab2ee57d1fe180d2a7e57c10a9cbf57774b49c9
kmh <kylemhouseholder@gmail.com>
2025-03-30 23:02:23 -0500

    renames, added code to calculator, plan file

--- python/test.py
+++
@@ -1,8 +0,0 @@
-import math
-
-# Density altitude calculator
-amsl = int(input("Enter current altitude (ft): "))
-baro = float(input("Enter barometer setting (\"Hg): "))
-temp = float(input("Enter temperature (°C): "))
-
-
