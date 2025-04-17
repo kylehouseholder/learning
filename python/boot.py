9e9781695cb94e85903fcdaddea25ba34ba4abaa
house <36007123+kylehouseholder@users.noreply.github.com>
2025-04-17 15:35:35 -0500

    Update boot.py

--- python/boot.py
+++ python/boot.py
@@ -1,12 +1,8 @@
-def new_resizer(max_width, max_height):
-    
-    def mins(min_width = 0, min_height = 0):
-        if min_width > max_width or min_height > max_height:
-            raise Exception("minimum size cannot exceed maximum size")
-        def size(width, height):
+from functools import lru_cache
 
-            return width, height
-            
-        return size
-        
-    return mins
+@lru_cache()
+def is_palindrome(word):
+    if word[0] == word[-1]:
+        newWord = word
+        if is_palindrome(word[1:-2]):
+            return True
